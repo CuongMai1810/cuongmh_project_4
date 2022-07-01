8a15a3332d0c82bc2c1ddb4c98d8f7001ca333c3
CuongMai1810 <hoangcuongmai1810@gmail.com>
2022-07-01 15:09:38 +0700

    - add project 4

---
+++ c4-final-project-starter-code/client/src/config.ts
@@ -0,0 +1,9 @@
+
+const apiId = 'b5u6rvhxj7'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-9kypell8.us.auth0.com',            // Auth0 domain
+  clientId: 'ce7mZYSYft9VS8z5tXUlZMIw3tucSOQu',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
